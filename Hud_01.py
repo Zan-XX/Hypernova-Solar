d40b1fad49c5a74b731c6c3a197c877e66b186e4
maxsyrett <57814515+maxsyrett@users.noreply.github.com>
2019-11-20 16:09:22 -0500

    Add files via upload

--- Hud_01.py
+++ Hud_01.py
@@ -1,31 +1,19 @@
-import Logger
+import HudInfo
 from tkinter import *
 from tkinter import ttk
-from datetime import *
 
 
 def update():
-    global temp1
-    global temp2
-    f = open('Hud.log', 'r')
-    f1 = f.readlines()
-    line = f1[-1].split(' ')
-    top = f1[0].split('-')
-    ttk.Label(mainframe, text=line[4]+' MPH').grid(column=1, row=2)
-    ttk.Label(mainframe, text=line[6]+'%').grid(column=2, row=2)
-    ttk.Label(mainframe, text=line[8]+u'\u00b0F').grid(column=3, row=2)
-    ttk.Label(mainframe, text=line[10]+u'\u00b0F').grid(column=4, row=2)
-    now = datetime.strptime(top[0], '%b/%d/%y %H:%M:%S ')
-    if now < time:
-        Logger.clearLine()
+    info = HudInfo.getHudInfo()
+    ttk.Label(mainframe, text=info[0]).grid(column=1, row=2)
+    ttk.Label(mainframe, text=info[1]).grid(column=2, row=2)
+    ttk.Label(mainframe, text=info[2]).grid(column=3, row=2)
+    ttk.Label(mainframe, text=info[3]).grid(column=4, row=2)
     root.after(300, update)
 
 
 root = Tk()
 root.title('Hud')
-temp1 = StringVar()
-temp2 = StringVar()
-time = datetime.now() - timedelta(hours=1)
 mainframe = ttk.Frame(root, padding='3 3 12 12')
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
@@ -37,5 +25,4 @@ ttk.Label(mainframe, text='Temp 2').grid(column=4, row=1)
 for child in mainframe.winfo_children():
     child.grid_configure(padx=20, pady=5)
 update()
-Logger.clearLine()
 root.mainloop()
--- Logger.py
+++ Logger.py
@@ -4,7 +4,7 @@ logging.basicConfig(filename='Hud.log', filemode='a+', format='%(asctime)s - %(m
 
 
 def log(speed, charge, temp1, temp2):
-    logRow = 'Speed: '+speed+' Charge: '+charge+' Temp1: '+temp1+' Temp2: '+temp2+' '
+    logRow = 'Speed: ' + speed + ' Charge: ' + charge + ' Temp1: ' + temp1 + ' Temp2: ' + temp2 + ' '
     logging.error('%s', logRow)
 
 
@@ -14,5 +14,6 @@ def clearLine():
     with open('Hud.log', 'w') as z:
         z.writelines(data[1:])
 
-for i in range(20):
-   log('55', '98', '70', '72')
+
+for i in range(2):
+    log('60', '100', '90', '92')
