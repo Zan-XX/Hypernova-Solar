c11c7348d5c248c4125403b48ff7e565678312e2
Hex_Turtle <57814515+maxsyrett@users.noreply.github.com>
2020-09-18 15:39:17 -0400

    Add files via upload

---
+++ canInterface.py
@@ -0,0 +1,43 @@
+import can
+import logging
+from datetime import *
+logging.basicConfig(filename='Hud.log', filemode='a+', format='%(asctime)s - %(message)s', datefmt='%b/%d/%y %H:%M:%S')
+
+
+def canSend(bus, aId, msg):
+    msg = can.Message(arbitration_id=aId, data=[msg])
+    bus.send(msg)
+
+
+def canReceive(bus):
+    msg = bus.recv(0.1)  # time for refresh
+    log(msg)
+    return msg
+
+
+def log(data):
+    logRow = 'TestInt: ' + str(data) + ' '
+    logging.error('%s', logRow)
+
+
+def clearLine():
+    with open('Hud.Log', 'r') as x:
+        data = x.read().splitlines(True)
+    with open('Hud.log', 'w') as z:
+        z.writelines(data[1:])
+
+
+def getHudInfo():
+    info = []
+    f = open('Hud.log', 'r')
+    f1 = f.readlines()
+    line = f1[-1].split(' ')
+    info.append(line[4])
+    for dLine in f1:
+        top = dLine.split('-')
+        now = datetime.strptime(top[0], '%b/%d/%y %H:%M:%S ')
+        if now < datetime.now() - timedelta(hours=1):
+            clearLine()
+        else:
+            break
+    return info
