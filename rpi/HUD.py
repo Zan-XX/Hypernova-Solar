09570e8dfaa538ea0a4f79027d135d8d400dfae1
maxsyrett <max.r.syrett@gmail.com>
2020-10-09 17:46:13 -0400

    Update HUD.py

--- rpi/HUD.py
+++ rpi/HUD.py
@@ -2,60 +2,71 @@
 # Python 3.7.3
 
 from tkinter import Tk, Label, Button, StringVar
+from collections import OrderedDict
 import can
 import canInterface
 
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate='500000')
+timeout = OrderedDict([(0x102, 0),  	# ordered dictionary to count missed packets for each sensor
+                       (0x104, 0)])     # (can_id, number missed)
 
 
 class CAN:
-	def __init__(self, master):
-		self.master = master
+    def __init__(self, master):
+        self.master = master
 
         # Make window fullscreen 
-		#master.attributes('-fullscreen', True) #Start in fullscreen with no borders
+        # master.attributes('-fullscreen', True) #Start in fullscreen with no borders
 
-		#Column 0
-		self.int = Label(master, text="Test int")
-		self.int.grid(row=0, column=0)
+        # Column 0
+        self.int = Label(master, text="Test int")
+        self.int.grid(row=0, column=0)
 
-		self.int_text = StringVar()
-		self.int_text.set('---')
-		self.int_label = Label(master, textvariable=self.int_text)
-		self.int_label.grid(row=1, column=0)
+        self.int_text = StringVar()
+        self.int_text.set('---')
+        self.int_label = Label(master, textvariable=self.int_text)
+        self.int_label.grid(row=1, column=0)
 
-		#Column 1
-		self.temp1 = Label(master, text="Temp 1")
-		self.temp1.grid(row=0, column=1)
+        # Column 1
+        self.temp1 = Label(master, text="Temp 1")
+        self.temp1.grid(row=0, column=1)
 
-		self.temp1_text = StringVar()
-		self.temp1_text.set('---')
-		self.temp1_label = Label(master, textvariable=self.temp1_text)
-		self.temp1_label.grid(row=1, column=1)
+        self.temp1_text = StringVar()
+        self.temp1_text.set('---')
+        self.temp1_label = Label(master, textvariable=self.temp1_text)
+        self.temp1_label.grid(row=1, column=1)
 
-		#Exit button since there is no window border
-		self.exit = Button(master, text="Quit", command=master.destroy)
-		self.exit.grid(row=0,column=2)
+        # Exit button since there is no window border
+        self.exit = Button(master, text="Quit", command=master.destroy)
+        self.exit.grid(row=0, column=2)
 
-	def update_field(self, data, can_id, byteorder='big'): #Update field based on CAN ID
-		num = int.from_bytes(data, byteorder)
-		if can_id == 0x102:
-			self.temp1_text.set("%02.1f" % (num / 10))
-			print('updated temp')
-		elif can_id == 0x104:
-			self.int_text.set("%03d" % num)
-			print('updated int')
+    def update_field(self, data, can_id, byteorder='big'):  # Update field based on CAN ID
+        num = int.from_bytes(data, byteorder)
+        if can_id == 0x102:
+            if timeout[can_id] > 5:
+                self.temp1_text.set("")
+                return
+            self.temp1_text.set("%02.1f" % (num / 10))
+            print('updated temp')
+        elif can_id == 0x104:
+            if timeout[can_id] > 5:
+                self.int_text.set("---")
+                return
+            self.int_text.set("%03d" % num)
+            print('updated int')
+
 
 def refresh():
-	print('--------------')
-	while True:
-		msg = bus.recv(timeout=0.1)
-		if msg is None:
-			print('break')
-			break
-		print(int.from_bytes(msg.data, byteorder='big'))
-		can.update_field(msg.data, msg.arbitration_id)
-	root.after(500, refresh)
+    print('--------------')
+    while True:
+        msg = bus.recv(timeout=0.1)
+        if msg is None:
+            print('break')
+            break
+        print(int.from_bytes(msg.data, byteorder='big'))
+        can.update_field(msg.data, msg.arbitration_id)
+    root.after(500, refresh)
+
 
 root = Tk()
 can = CAN(root)
