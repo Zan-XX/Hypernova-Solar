8af020af14e636309112bf4faf0ff6ab1d2da230
Zayn Hasan <zaynhasan4@gmail.com>
2021-09-03 16:50:57 -0400

    refspec main does not match any

--- UpdateGit.sh
+++ UpdateGit.sh
@@ -2,4 +2,4 @@ git add .
 git pull
 read -p "Please enter a commit name or phrase: " name; \
 git commit -m "$name"
-git push -u https://github.com/maxsyrett/Hypernova-Solar.git main
+git push -u https://github.com/maxsyrett/Hypernova-Solar.git master
