5c6bbaa4e713767511b3e3832abd93c1a4a6bb97
Zayn Hasan <zaynhasan4@gmail.com>
2021-09-03 16:49:15 -0400

    Made some files to help with pushing, pulling, and getting started

--- UpdateGit.sh
+++ UpdateGit.sh
@@ -2,4 +2,4 @@ git add .
 git pull
 read -p "Please enter a commit name or phrase: " name; \
 git commit -m "$name"
-git push -u origin main
+git push -u https://github.com/maxsyrett/Hypernova-Solar.git main
