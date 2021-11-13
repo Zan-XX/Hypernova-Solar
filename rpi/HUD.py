e1422c0998333b7b49ad670e4bcddf8ba99d661d
Zack W <71153497+zattak3000@users.noreply.github.com>
2021-11-13 04:28:18 -0500

    Initial rewrite commit

--- rpi/HUD.py
+++ rpi/HUD.py
@@ -1,92 +1,192 @@
-# usr bin python3
-# Python 3.7.3
-
-from tkinter import Tk, Label, Button, StringVar
-import logger as log
+import threading
+from tkinter import *
+from tkinter.font import Font
 import can
-
-# Config
-bus = can.interface.Bus(bustype='virtual', channel='can0', bitrate='500000')
-timeout = {  # dictionary to count missed packets for each sensor
-    0x102: 0,  # (can_id, number missed)
-    0x104: 0
-}
-
-
-# GUI
-class GUI:
-    def __init__(self, master):
-        self.master = master
-
-        # Make window fullscreen
-        # master.attributes('-fullscreen', True) #Start in fullscreen with no borders
-
-        # Column 0
-        self.int = Label(master, text="Test int")
-        self.int.grid(row=0, column=0)
-
-        self.int_text = StringVar()
-        self.int_text.set('---')
-        self.int_label = Label(master, textvariable=self.int_text)
-        self.int_label.grid(row=1, column=0)
-
-        # Column 1
-        self.temp1 = Label(master, text="Temp 1")
-        self.temp1.grid(row=0, column=1)
-
-        self.temp1_text = StringVar()
-        self.temp1_text.set('---')
-        self.temp1_label = Label(master, textvariable=self.temp1_text)
-        self.temp1_label.grid(row=1, column=1)
-
-        # Exit button since there is no window border
-        self.exit = Button(master, text="Quit", command=master.destroy)
-        self.exit.grid(row=0, column=2)
-
-
-# CAN Logic
-def update_field(self, data, can_id, byteorder='big'):  # Processes data based on can_id
-    num = int.from_bytes(data, byteorder)
-
-    if can_id == 0x102:  # checks message CAN id
-        self.temp1_text.set("%02.1f" % (num / 10))  # logic based on the can type of message and/or data processing
-        timeout[can_id] = 0  # reset timeout
-        log.log("temp1", "%02.1f" % (num / 10))  # log data
-
-    if can_id == 0x104:
-        self.int_text.set("%03d" % num)
-        timeout[can_id] = 0
-        log.log("int_text", "%03d" % num)
-
-
-def check_timeout(self):  # Checks to see if a sensor timed out and if it does set it as "---"
-    for i in timeout:
-        timeout[i] += 1
-    if timeout[0x102] > 5:
-        self.temp1_text.set("---")
-        log.log("temp1", "--- Not Responding ---")
-    elif timeout[0x104] > 5:
-        self.int_text.set("---")
-        log.log("int_text", "--- Not Responding ---")
-
-
-# refresh can messages and HUD
-def refresh():
-    print('--------------')
-    while True:
-        msg = bus.recv(timeout=0.1)
-        if msg is None:
-            print('break')
-            break
-        print(int.from_bytes(msg.data, byteorder='big'))
-        can.update_field(msg.data, msg.arbitration_id)
-        can.check_timeout()
-        log.clearOld()
-    root.after(100, refresh)
-
+from hudextras import Gauge, Battery
 
 root = Tk()
-can = GUI(root)
-root.after_idle(refresh)
+
+mph_var = IntVar()
+mph_label_var = StringVar()
+
+batt_var = IntVar()
+temp_label_var = StringVar()
+
+
+# TODO Rewrite this whole function its not that great
+# * Avoid updating unchanged fields?
+# * Possibly offload IntVar setting to function in tkinter event loop to avoid spam?
+
+def update_display(sensors):
+
+    # If a sensor has not been heard from, report an error
+    if sensors[0x102].missed > 10:
+        temp_label_var.set("TEMPERATURE ERROR!")
+
+    if sensors[0x104].missed > 10:
+        # TODO Add battery error action
+        pass
+
+    if sensors[0x106].missed > 10:
+        mph_label_var.set("SPEED ERROR!")
+        mph_var.set(0)
+
+    # Update tkinter variables with new data
+    temp = int.from_bytes(sensors[0x102].data, 'big')
+    temp_label_var.set(f"Temp: {temp} F")
+
+    level = int.from_bytes(sensors[0x104].data, 'big')
+    batt_var.set(level)
+
+    speed = int.from_bytes(sensors[0x106], 'big')
+    mph_var.set(speed)
+    mph_label_var.set(f"{speed} MPH")
+
+
+# TODO Add proper sensor IDs
+
+def can_handler():
+
+    bus = can.Bus('can0', bustype='virtual', bitrate='500000')
+
+    # Dictionary for last data recieved and missed packet tally
+    sensors = {
+        # Temperature Sensor
+        0x102: {
+            'data': 0,
+            'missed': 0
+        },
+
+        # Battery Sensor
+        0x104: {
+            'data': 0,
+            'missed': 0
+        },
+
+        # Speed Sensor
+        0x106: {
+            'data': 0,
+            'missed': 0
+        }
+    }
+
+    while True:
+        # Recieve a packet
+        msg = bus.recv()
+
+        id = msg.arbitration_id
+        data = msg.data
+
+        # If the sensor is in the list
+        if id in sensors:
+            # Set that sensor missed count to 0
+            sensors[id].missed = 0
+        else:
+            # Don't do anything else if it isn't in the list
+            continue
+
+        # Increase the missed count for all sensors by 1
+        for i in sensors:
+            sensors[i].missed += 1
+
+        # Update the sensor data field in the dictionary
+        sensors[id].data = data
+
+        # Pass updates to tkinter
+        update_display(sensors)
+
+
+# Start the can bus thread as a daemon so it is killed when the window is closed
+backend = threading.Thread(target=can_handler, daemon=True, )
+backend.start()
+
+
+# TODO Write button callback handlers
+# * Impliment StringVars for various labels
+
+class GUI:
+    def __init__(self, master: Tk, speed: IntVar, battery: IntVar) -> None:
+        '''
+        Create new instance of the Hypernova Solar GUI
+
+        :param master: Tk instance to add GUI to
+        :param speed: IntVar used for the speed gauge
+        :param battery: IntVar used for the battery level
+        '''
+        self.master = master
+        self.speed = speed
+        self.battery = battery
+
+        self.top = Frame(root, relief=SUNKEN, borderwidth=5)
+        self.__create_top_widgets(self.top)
+        self.top.pack(fill=BOTH)
+
+        self.bottom = Frame(root, relief=SUNKEN, borderwidth=5)
+        self.__create_bottom_widgets(self.bottom)
+        self.bottom.pack(expand=TRUE, fill=BOTH)
+
+    def __create_top_widgets(self, frame: Frame):
+        '''
+        Create and arrange the widgets for the top frame
+
+        :param frame: The top frame instance
+        '''
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+
+        power = Battery(frame, 50, 25, width=400, variable=self.battery)
+        powerlabel = Label(
+            frame, text="Voltage 100 V\nTemperature: 30 C", font=Font(size=20))
+
+        power.grid(column=0, row=0, padx=10, pady=10)
+        powerlabel.grid(column=0, row=1)
+
+        speed = Gauge(frame, 9, 100, width=400, variable=self.speed)
+        speedlabel = Label(frame, text="0 MPH", font=Font(size=40))
+
+        speed.grid(column=1, row=0, padx=10, pady=10)
+        speedlabel.grid(column=1, row=1)
+
+    def __create_bottom_widgets(self, frame: Frame):
+        '''
+        Create and arrange the widgets for the bottom frame
+
+        :param frame: The bottom frame instance
+        '''
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        frame.rowconfigure(0, weight=1)
+        frame.rowconfigure(1, weight=1)
+
+        button1 = Button(frame, text="Button 1", font=Font(size=20))
+        button2 = Button(frame, text="Button 2", font=Font(size=20))
+        button3 = Button(frame, text="Button 3", font=Font(size=20))
+        button4 = Button(frame, text="Button 4", font=Font(size=20))
+
+        button1.grid(column=0, row=0, padx=10, pady=10, sticky=NSEW)
+        button2.grid(column=1, row=0, padx=10, pady=10, sticky=NSEW)
+        button3.grid(column=0, row=1, padx=10, pady=10, sticky=NSEW)
+        button4.grid(column=1, row=1, padx=10, pady=10, sticky=NSEW)
+
+# Start app fullscreen
+# root.attributes('-fullscreen', True)
+
+
+interface = GUI(root, mph_var, batt_var)
+
+# Recursively makes all backgrounds of frames green and widgets red for tkinter debugging
+# def debugcolor(top):
+#     for i in top.winfo_children():
+#         if type(i) is Frame:
+#             i.configure(bg="green")
+#             debugcolor(i)
+#         else:
+#             i.configure(bg="red")
+
+# debugcolor(root)
+
+# Force minimum window size once widgets are laid out
+root.update()
+root.minsize(root.winfo_width(), root.winfo_height())
 
 root.mainloop()
